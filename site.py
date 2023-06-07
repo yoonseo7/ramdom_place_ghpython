be082a3b4d50c0f4b57af188f770ab6da5b457c8
고윤서 <yoonseo77777@naver.com>
2023-06-08 05:22:44 +0900

    Update site.py

--- site.py
+++ site.py
@@ -3,8 +3,12 @@ import matplotlib.pyplot as plt
 from matplotlib.path import Path
 from scipy.spatial import ConvexHull
 
+hig = 89
+wid = 30
+
 # 지정된 점들
-points = np.array([[0, 0], [0, 9], [55, 0], [48, 9]])
+# points = np.array([[0, 0], [0, 9], [55, 0], [48, 9]])
+points = np.array([[84, 14], [88, 4], [0, 0], [0, 29]])
 
 # ConvexHull로 주어진 점들의 볼록 껍질을 찾는다
 hull = ConvexHull(points)
@@ -13,11 +17,11 @@ hull = ConvexHull(points)
 hull_path = Path(points[hull.vertices])
 
 # 108*47 크기의 2차원 리스트를 생성한다
-arr = [[0]*10 for _ in range(56)]
+arr = [[0]*wid for _ in range(hig)]
 
 # 리스트를 순회하며 각 요소가 볼록 껍질 내부에 있는지 판별한다
-for i in range(56):
-    for j in range(10):
+for i in range(hig):
+    for j in range(wid):
         if hull_path.contains_point((i, j)):
             arr[i][j] = 0
         else:
